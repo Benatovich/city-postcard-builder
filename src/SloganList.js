cd66cb252faaa55c105d0e5ffd85f9a4d44d8fa8
Benatovich <sambenatovich@gmail.com>
2022-01-31 17:35:25 -0500

    getting an error that says 'slogans.map is not a function.' about to try something different

--- src/SloganList.js
+++ src/SloganList.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import Slogan from './Slogan';
 
 export default function SloganList({ slogans }) {
+  const slogansEl = slogans.map((slogan, i) => {
+    return (
+      <Slogan key={`${slogan} + ${i}`} {...slogan} />
+    );
+  }
+  );
+  
   return (
     <section>
       <h2>Your slogans:</h2>
       {/* This kind of component should be familiar by now. It takes in (as props) an array of slogans */}
       {/* Then map over those slogans and for each slogan, render a Slogan component. Supply a key and a slogan as props to each Slogan component */}
+      <ul className='slogan-list'>
+        {slogansEl}
+      </ul>
     </section>
   );
 }
