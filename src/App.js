6e074d0d2d9f876aa914c5e68d70fbc26a5851cf
Benatovich <sambenatovich@gmail.com>
2022-01-31 15:24:36 -0500

    city component set up and working

--- src/App.js
+++ src/App.js
@@ -13,10 +13,19 @@ function App() {
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
   // you'll need to track a city name, which starts as the city name of your choice.
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
+  const [skylineId, setSkylineId] = useState(1);
+  const [waterfrontId, setWaterfrontId] = useState(1);
+  const [castleId, setCastleId] = useState(1);
+  const [cityName, setCityName] = useState('Arrakeen');
+  const [slogans, setSlogans] = useState('Come for the spice, stay for the Shai-Hulud');
 
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
+      <City
+        skylineId={skylineId}
+        waterfrontId={waterfrontId}
+        castleId={castleId} />
       <h1>
         {/* dynamically update the city name here using state */}
         Welcome to beautiful Portland!
