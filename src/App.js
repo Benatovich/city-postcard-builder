8602d13c359882f349693eac9022a8b5bdd6dae9
Benatovich <sambenatovich@gmail.com>
2022-01-31 16:00:28 -0500

    I think the sloganform component is working properly but I need to finish the sloganlist component to be certain

--- src/App.js
+++ src/App.js
@@ -45,7 +45,10 @@ function App() {
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
         {/* here, the SloganList component takes the array of slogans that lives in state */}
-        <SloganForm setSlogans={setSlogans} />
+        <SloganForm 
+          setSlogans={setSlogans}
+          slogans={slogans} />
+        <SloganList slogans={slogans} />
       </div>
     </div>
   );
